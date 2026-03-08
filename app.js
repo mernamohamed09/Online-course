4d8fc0f416f3fb6f86f474a50475acce7fdbbbc3
Merna_saied <Merna09mohamed@gmail.com>
2026-03-08 15:10:25 +0200

    phase1

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+async function dbConnection(){ 
+try {
+    
+    await mongoose.connect(process.env.DB_URL);
+    console.log("connected");
+    
+} catch (error) {
+   console.log("error");
+    
+}
+}
+dbConnection();
+
+app.listen(port, ()=>{
+    console.log(`Server is running at port ${port}`);
+    
+});
